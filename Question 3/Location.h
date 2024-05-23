06226cabe1e98ab389096827192d762266e89dfb
Adil Farid <adilfarid1389@gmail.com>
2024-05-22 21:37:23 -0400

    Created the pointer  and reference swap functions for question 2 and question 3.

---
+++ Question 3/Location.h
@@ -0,0 +1,12 @@
+/*Class*/
+class Location {
+
+public:
+	int longitude;
+	int latitude;
+
+};
+/*Functions Prototypes*/
+void Swap(Location* l1, Location* l2);
+
+void Swap(Location& l1, Location& l2);
